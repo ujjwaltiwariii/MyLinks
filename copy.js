8a230b35c4dd8d86abe0aedcbf5715f41a668f30
Ujjwal Tiwari <ujjwal.tiwari070@gmail.com>
2023-05-09 22:47:34 +0530

    Initial Commit

---
+++ copy.js
@@ -0,0 +1,56 @@
+const shareButtons = document.querySelectorAll('.title-share-button')
+
+const sharebtn = document.querySelectorAll('.share-button')
+
+
+function getCurrentURL () {
+    return window.location.href
+  }
+
+  const url = getCurrentURL()
+
+sharebtn[0].addEventListener('click',sharee)
+
+
+function sharee(e){
+    if(navigator.share){
+        navigator.share({
+            text:'Sharing Links',
+            url:url
+        }).then(()=>{
+            console.log("thanks for sharing");
+        })
+        .catch(console.error)
+    }
+    else{
+        alert("Not Supported")
+    }
+
+
+}
+
+console.log(url)
+
+
+
+
+
+
+
+
+async function copyText(e){
+    e.preventDefault()
+    const link=this.getAttribute('link')
+    console.log(link)
+    try{
+        await navigator.clipboard.writeText(link)
+        alert("Link Copied")
+    }
+    catch(err){
+        console.error(err)
+    }
+
+
+}
+
+shareButtons.forEach(shareButtons =>  shareButtons.addEventListener('click',copyText))
